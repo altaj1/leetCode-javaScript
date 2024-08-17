3efe926a02a14abb4ad4d5e9304fb68a5c6e78af
altaj1 <altaj1019@gmail.com>
2024-08-17 20:25:56 +0600

    solve interval cancellation

--- problem2.js
+++ problem2.js
@@ -48,16 +48,44 @@
   
 //   addTwoPromises(promise1, promise2)
 //     .then(console.log); // Output: 7
-async function sleep(millis) {
-    return new Promise (resolve => {
-         setTimeout(()=>{
-             resolve(millis);
-         },millis);
-    });
- }
+// async function sleep(millis) {
+//     return new Promise (resolve => {
+//          setTimeout(()=>{
+//              resolve(millis);
+//          },millis);
+//     });
+//  }
  
   
-   let t = Date.now()
-   sleep(100).then(() => console.log(Date.now() - t)) // 100
+//    let t = Date.now()
+//    sleep(100).then(() => console.log(Date.now() - t)) // 100
   
+const cancellable = function(fn, args, t) {
+    // cancelFn function//
+    const cancelFn = function (){
+      clearTimeout(timer);
+  };
+  const timer = setTimeout(()=>{
+      fn(...args)
+  }, t);
+  return cancelFn ;
+};
+
+
+
+  const result = []
+
+ const fn = (x) => x * 5
+ const args = [2], t = 20, cancelT = 50
+
+ const log = (...argsArr) => {
+     result.push(fn(...argsArr))
+ }
+      
+ const cancel = cancellable(fn, args, t);
+           
+  setTimeout(() => {
+     cancel()
+     console.log(result) // [{"time":20,"returned":10}]
+  }, cancelT)
  
