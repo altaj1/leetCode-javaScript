60e3b960f74e53d30cd18a6de5800f524bdcfde9
altaj1 <altaj1019@gmail.com>
2024-08-19 02:54:12 +0600

    Product of Array Except Self

--- index.js
+++ index.js
@@ -78,9 +78,30 @@
 // const result = reverseVowels("leetcode")
 // console.log(result)
 
-function reverseWords (s){
-  let word = s.trim().split(/\s+/);
-  return word.reverse().join(' ');
+// function reverseWords (s){
+//   let word = s.trim().split(/\s+/);
+//   return word.reverse().join(' ');
+// }
+// const result = reverseWords("the sky is blue");
+// console.log(result)
+function productExceptSelf (nums){
+  const n = nums.length;
+  const result = new Array(n).fill(1)
+ 
+  let prefix = 1;
+  for (let i = 0; i <n; i++) {
+    result[i] = prefix;
+    prefix *= nums[i]
+    
+  }
+  
+  let suffix = 1
+  for (let i = n - 1; i >=0; i --) {
+    result[i] = suffix;
+    suffix *= nums[i]
+    
+  }
+  return result
 }
-const result = reverseWords("the sky is blue");
+const result = productExceptSelf([1, 2, 3, 4])
 console.log(result)
