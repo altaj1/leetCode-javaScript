b35b8df56f085f072f1ebb31314cd97cc944b0aa
altaj1 <altaj1019@gmail.com>
2024-08-18 21:19:22 +0600

    Kids With the Greatest Number of Candies

--- index.js
+++ index.js
@@ -19,11 +19,23 @@
 // let word1 = "ab";
 // let word2 = "pqrs";
 // let result = mergeAlternately(word1, word2);
-var mergeAlternately = function(word1, word2) {
-    let result = '';
-     for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-       if (i < word1.length) result += word1[i];
-       if (i < word2.length) result += word2[i];
-     }
-     return result;
-   };
+// var mergeAlternately = function(word1, word2) {
+//     let result = '';
+//      for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+//        if (i < word1.length) result += word1[i];
+//        if (i < word2.length) result += word2[i];
+//      }
+//      return result;
+//    };
+
+function kidsWithCandies(candies, extraCandies){
+  const maxCandies = Math.max(...candies)
+  return candies.map(candy=> candy + extraCandies >= maxCandies)
+ 
+}
+
+// উদাহরণ
+const candies1 = [4,2,1,1,2]
+const extraCandies1 =1
+const result = kidsWithCandies(candies1, extraCandies1)
+console.log(result)
