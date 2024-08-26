ea4fc2cdc9ce5849fdabd66d495bb58275b99eb6
altaj1 <altaj1019@gmail.com>
2024-08-26 21:24:40 +0600

    Palindrome string solve

--- index.js
+++ index.js
@@ -139,13 +139,48 @@
 
 // }
 // const resut = isSubsequence("abc", "ahbgdc")
-function palindromeFun (val){
-    if (val < 0) {
-        return false
+// function palindromeFun (val){
+//     if (val < 0) {
+//         return false
+//     }
+//     const valueStr = val.toString()
+//     const rev = valueStr.split('').reverse().join('')
+//     return rev == val
+// }
+// const resut = palindromeFun(121)
+// console.log(resut)
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (!s) {
+        return "";
     }
-    const valueStr = val.toString()
-    const rev = valueStr.split('').reverse().join('')
-    return rev == val
-}
-const resut = palindromeFun(121)
-console.log(resut)
+
+    function expandAroundCenter(s, left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const odd = expandAroundCenter(s, i, i);
+        const even = expandAroundCenter(s, i, i + 1);
+        const max_len = Math.max(odd, even);
+
+        if (max_len > end - start) {
+            start = i - Math.floor((max_len - 1) / 2);
+            end = i + Math.floor(max_len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);    
+};
+const result = longestPalindrome("babad")
